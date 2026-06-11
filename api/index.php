9bd2a425e32612a82c5bfec3b60d69eaea67b827
nakadaalpha <nakadaalpha@gmail.com>
2026-06-11 22:31:24 +0700

    fix: override bootstrap cache paths for vercel

--- api/index.php
+++ api/index.php
@@ -23,6 +23,21 @@ try {
     
     putenv("CACHE_STORE=database");
     $_ENV['CACHE_STORE'] = "database";
+
+    putenv("APP_SERVICES_CACHE={$storagePath}/bootstrap/cache/services.php");
+    $_ENV['APP_SERVICES_CACHE'] = "{$storagePath}/bootstrap/cache/services.php";
+    
+    putenv("APP_PACKAGES_CACHE={$storagePath}/bootstrap/cache/packages.php");
+    $_ENV['APP_PACKAGES_CACHE'] = "{$storagePath}/bootstrap/cache/packages.php";
+    
+    putenv("APP_CONFIG_CACHE={$storagePath}/bootstrap/cache/config.php");
+    $_ENV['APP_CONFIG_CACHE'] = "{$storagePath}/bootstrap/cache/config.php";
+    
+    putenv("APP_ROUTES_CACHE={$storagePath}/bootstrap/cache/routes.php");
+    $_ENV['APP_ROUTES_CACHE'] = "{$storagePath}/bootstrap/cache/routes.php";
+    
+    putenv("APP_EVENTS_CACHE={$storagePath}/bootstrap/cache/events.php");
+    $_ENV['APP_EVENTS_CACHE'] = "{$storagePath}/bootstrap/cache/events.php";
     
     // Create required storage directories on Vercel
     $directories = [
@@ -30,6 +45,7 @@ try {
         $storagePath . '/framework/sessions',
         $storagePath . '/framework/views',
         $storagePath . '/logs',
+        $storagePath . '/bootstrap/cache',
     ];
 
     foreach ($directories as $dir) {
